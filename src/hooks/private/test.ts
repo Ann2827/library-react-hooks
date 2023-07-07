b0575a783b515ab8ff069431f139a13fc1b50f11
Bystrova Ann <ann.bystrova96@mail.ru>
2023-07-07 07:33:01 +0300

    feat: private pages

---
+++ src/hooks/private/test.ts
@@ -0,0 +1,24 @@
+// {
+//         path: ROUTE_CODE,
+//         element: <PrivateOutlet path={ROUTE_FULL_RESET_TOTP_CODE} />,
+//         children: [
+//           {
+//             path: '',
+//             element: <ResetTotpCode />,
+//           },
+//         ],
+//       },
+
+// export const privatePagesConfig: PagesRulesT = {
+//   // Формы
+//   [ROUTE_FULL_PASSWORD]: {
+//     paths: [ROUTE_FORMS, ROUTE_PASSWORD],
+//     available: false,
+//     redirect: ROUTE_SECURITY,
+//   },
+//   [ROUTE_FULL_2FA]: {
+//     paths: [ROUTE_FORMS, ROUTE_2FA],
+//     available: false,
+//     redirect: ROUTE_SECURITY,
+//   },
+// }
