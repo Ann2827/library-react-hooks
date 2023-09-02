64548c468832ff473562dab1c2f61d6902cd2b69
Bystrova Ann <ann.bystrova96@mail.ru>
2023-09-02 18:02:36 +0300

    feat: link tsconfig with eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
+import bundleAnalyzer from 'rollup-plugin-bundle-analyzer';
+
+const args = process.argv.slice(2);
+const getArgvParameter = (argv, key) => {
+  const value = argv.find((item) => item.includes(key))?.split('=')[1] || '';
+  return value.trim().replace(/"/g, '').replace(/'/g, '');
+};
+
+const plugins = [];
+const analyze = getArgvParameter(args, '--analyze');
+if (analyze) plugins.push(bundleAnalyzer());
 
 export default [
   {
@@ -14,12 +25,17 @@ export default [
     external: ['react'],
     plugins: [
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        include: ['src/**/**/*.ts'],
+        exclude: ['src/**/**/*.test.ts', '__mocks__'],
+      }),
       terser({
         output: {
           comments: false,
         },
       }),
+      ...plugins,
     ],
   },
 ];
